165c5a5d8227ad87d47dc3e069c3912e57942b06
monish sharma <monish@xamidea.in>
2019-12-04 09:30:13 +0530

    Updates

---
+++ precache-manifest.24640d2825ab71e8d9227d65ac08915f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b7d4bbe806b782384ff2b7fbfacfff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb566480ed7fa79eac05",
+    "url": "/static/css/main.6923ab95.chunk.css"
+  },
+  {
+    "revision": "2d6703da1b71d6a41fe6",
+    "url": "/static/js/2.36454b1a.chunk.js"
+  },
+  {
+    "revision": "fb566480ed7fa79eac05",
+    "url": "/static/js/main.eef46102.chunk.js"
+  },
+  {
+    "revision": "c66bd39318662d7ab0bb",
+    "url": "/static/js/runtime-main.2940ff0c.js"
+  }
+]);
